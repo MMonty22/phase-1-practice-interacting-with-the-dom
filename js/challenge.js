6544e742a42cf884d50babc940b60fcb6ba04b0f
Michael <michael.c.montgomery@gmail.com>
2022-03-09 19:48:35 -0600

    completed assignment

--- js/challenge.js
+++ js/challenge.js
@@ -3,17 +3,18 @@ let counter = document.querySelector('#counter')
 let number = 0
 
 function increase () {
+    console.log(number)
     if (number >= 0) {
         ++number
         counter.innerText = number
     }
 }
 
-function incrementCounter () {
-    setInterval(increase, 1000)   
-}
+let interval = setInterval(increase, 1000)
 
-incrementCounter();
+function incrementCounter () {
+    interval = setInterval(increase, 1000)
+}
 
 //Manually increment and decrement the counter using the plus and minus buttons.
 const plusButton = document.querySelector('#plus')
@@ -30,51 +31,61 @@ const heartButton = document.querySelector('#heart')
 heartButton.addEventListener('click', clickHeart)
 
 let numberLikes = 0
+let array = []
 
 function clickHeart () {
     ++numberLikes
     const likes = document.querySelector('.likes')
-    let newNumber = ''
-    const likeLists = document.createElement('li')
-    if (number !== newNumber) {
-        likeLists.innerText = `${number} has been liked ${numberLikes} times`
-        likes.appendChild(likeLists)
+    const li = document.createElement('li')
+    li.id = number
+    if (array.includes(number)) {
+        let matchingLi = document.getElementById(number)
+        matchingLi.innerText = `${number} has been liked ${numberLikes} times`
     }
-    else (likeLists.innerText = `${number} has been liked ${numberLikes} times`)
+    else {
+        numberLikes = 1
+        likes.appendChild(li)
+        li.innerText = `${number} has been liked ${numberLikes} times`
+        array.push(number)
+    }
 }
 
-//Pause the counter, which should:
 //pause the counter
 //disable all buttons except the pause button
 //switch the label on the button from "pause" to "resume"
+//Click the "restart" button to restart the counter and re-enable the buttons.
 const pauseButton = document.querySelector('#pause')
 
 pauseButton.addEventListener('click', pauseCounter)
+let paused = false
 
 function pauseCounter () {
-    counter = number
-    pauseButton.innerText = 'resume'
-    heartButton.disabled = true
-    plusButton.disabled = true
-    minusButton.disabled = true
-}
-
-//Click the "restart" button to restart the counter and re-enable the buttons.
-if (pauseButton.innerText === 'resume') {
-    pauseButton.addEventListener('click', function () {
+    if (!paused) {
+        paused = true
+        clearInterval(interval)
+        pauseButton.innerText = 'resume'
+        heartButton.disabled = true
+        plusButton.disabled = true
+        minusButton.disabled = true  
+    }
+    else {
+        paused = false
+        incrementCounter()
         pauseButton.innerText = 'pause'
         heartButton.disabled = false
         plusButton.disabled = false
         minusButton.disabled = false
-    })
+    }
 }
 
 //Leave comments on my gameplay, such as: "Wow, what a fun game this is."
 const commentBox = document.querySelector('#comment-form')
 commentBox.addEventListener('submit', submitComment)
 
-function submitComment(e, input) {
+function submitComment(e) {
     e.preventDefault()
     const comment = document.createElement('p')
-    comment.innerHTML = `${input}`
+    comment.innerText = `${e.target[0].value}`
+    const commentList = document.querySelector('#list')
+    commentList.appendChild(comment)
 }
